d9159442b9266d9bd8fa48df06e3d5abea570c95
seenevz <serneves@hotmail.com>
2019-01-03 12:24:55 +0000

    created win method

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,4 +1,52 @@
 class Battle < ActiveRecord::Base
     belongs_to :trainer_pokemon
     belongs_to :pokemon
+    after_create :draw_poke
+    after_create :trainer_poke
+
+    attr_accessor :trn_poke, :enem_poke
+
+    def attack(def_flag = false)
+
+        if def_flag == false
+            self.enem_poke.hp -= (self.trn_poke.attack - (self.enem_poke.defense * rand))
+            self.trn_poke.hp -= (self.enem_poke.attack - (self.trn_poke.defense * rand))
+        else
+            self.trn_poke.hp -= (self.enem_poke.attack / self.trn_poke.defense)
+        end
+        win?
+    end
+
+    def defense
+        self.trn_poke.defense += (self.trn_poke.defense * rand)
+        self.attack(true)
+    end
+
+    private
+    
+    def trainer_poke
+        @trn_poke = self.trainer_pokemon.pokemon
+    end
+
+    def draw_poke
+        @enem_poke = Pokemon.find(rand(1..151))
+        self.update(pokemon_id: self.enem_poke.id)
+    end
+
+    def win?
+        hp_trn = self.trn_poke.hp
+        hp_enem = self.enem_poke.hp
+        
+        if hp_trn <= 0 && hp_enem > 0
+            puts "lost"
+            false
+        elsif hp_trn > 0 && hp_enem <= 0 
+            puts "win"
+            true
+        else
+            puts "go again"
+        end
+        
+    end
+
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,15 @@
 require_relative '../config/environment'
 
+t1 = Trainer.create
+t2 = Trainer.create
+pk1 = Pokemon.find(rand * 100)
+pk2 = Pokemon.find(rand * 100)
+tp1 = TrainerPokemon.create(trainer_id: t1.id, pokemon_id: pk1.id )
+tp2 = TrainerPokemon.create(trainer_id: t2.id, pokemon_id: pk2.id )
+btl1 = Battle.create(trainer_pokemon_id: tp1.id)
+btl2 = Battle.create(trainer_pokemon_id: tp2.id)
 
 
-puts "HELLO WORLD"
+
+binding.pry
+puts "done!!"
