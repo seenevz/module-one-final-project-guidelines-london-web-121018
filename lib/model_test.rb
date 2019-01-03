73a96c87020f94fae2f3219bddc716b9e98d61bc
Dark-Raine <anderson-tomas@hotmail.com>
2019-01-03 16:46:14 +0000

    Trainer stat functions complete.

--- lib/model_test.rb
+++ lib/model_test.rb
@@ -4,9 +4,10 @@ require_relative '../config/environment'
 tp1 = TrainerPokemon.second
 t1 = Trainer.first
 p1 = Pokemon.first
-b1 = Battle.first
+b1 = Battle.new
 p2 = Pokemon.last
-p3 = Pokemon.second
+p3 = Pokemon.find(rand*151)
+
 
 binding.pry
 p 'eof'
